a49c3abb79696f2e9126839e976902db1e5216c7
David Agaybi <davidagaybi@gmail.com>
2018-09-11 23:42:55 -0400

    GraphQL with hardcoded data

--- schema.js
+++ schema.js
@@ -2,10 +2,48 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLSchema,
+    GraphQLInt,
     GraphQLList,
     GraphQLNonNull
 } =  require('graphql');
 
-module.exports = new GraphQLSchema({
+// Hardcoded Data
+const customers = [
+    {id: '1', name:'David Gabi', email:'dg@g.com', age: 42},
+    {id: '2', name:'David Aybi', email:'day@g.com', age:30},
+    {id: '3', name:'Davi Agabi', email:'da@g.com', age:23},
+]
 
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+    name:'Customer',
+    fields: () => ({
+        id:{type:GraphQLString},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        age: {type: GraphQLInt},
+    })
+})
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        customer: {
+            type: CustomerType,
+            args:{
+                id:{type: GraphQLString}
+            },
+            resolve(parentValue, args){
+                for(let i =0;i < customers.length;i++){
+                    if(customers[i].id == args.id){
+                        return customers[i];
+                    }
+                }
+            }
+        }
+    }
+    
+});
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
